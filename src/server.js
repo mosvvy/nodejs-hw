62bc7eb14503deddabc77c8ef6b5b3f1c5aa9371
ValeriiaM <moskalenkovvy@gmail.com>
2026-01-07 20:10:23 +0200

    adopt responces acc to the task

--- src/server.js
+++ src/server.js
@@ -42,36 +42,31 @@ app.use(
 );
 
 app.get('/notes', (req, res) => {
-  res
-    .status(200)
-    .json({
-      message: 'All notes retrieved',
-      data: [{ id: 1, msg: 'This is 1st note' }],
-    });
+  res.status(200).json({
+    message: 'Retrieved all notes',
+  });
 });
 
 app.get('/notes/:noteId', (req, res) => {
   const { noteId } = req.params;
-  res.status(200).json({ id: noteId, msg: `This is the note ${noteId}` });
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
 });
 
 app.get('/test-error', (req, res) => {
-  throw new Error('Something went wrong');
+  throw new Error('Simulated server error');
 });
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
+  res.status(404).json({
+    message: 'Route not found',
+  });
 });
 
 app.use((err, req, res, next) => {
   console.error(err);
 
-  const isProd = process.env.NODE_ENV === 'production';
-
   res.status(500).json({
-    message: isProd
-      ? 'Something went wrong. Please try again later.'
-      : err.message,
+    message: err.message,
   });
 });
 
